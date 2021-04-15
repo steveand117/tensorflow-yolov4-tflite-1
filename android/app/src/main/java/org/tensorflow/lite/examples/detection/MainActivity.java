c3b5a568f45b9bb22b993bd8dd821dac214e4c9d
Andrew Zhao <steveand117@gmail.com>
2021-04-15 18:05:56 -0400

    Added FP16 Road Sign models with correct configuration

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -74,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
 
-    private static final String TF_OD_API_MODEL_FILE = "yolov4-416-fp32.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "yolov4-416-42000-fp16.tflite";
 
-    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco.txt";
+    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/road_sign.txt";
 
     // Minimum detection confidence to track a detection.
     private static final boolean MAINTAIN_ASPECT = false;
